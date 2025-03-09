d11d91ae61170c708f10872f227f0f927c583ccd
malachibazar <malachibazar@gmail.com>
2025-03-09 16:06:42 -0400

    removed temp files

--- gunicorn_conf.py
+++
@@ -1,5 +0,0 @@
-# gunicorn_conf.py
-workers = 4  # Number of worker processes (adjust as needed)
-bind = "unix:/home/malachi/json_to_pydantic/app.sock"
-timeout = 120  # Timeout for requests (in seconds)
-keepalive = 2  # Keep-alive connections
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from json-to-pydantic!")
-
-
-if __name__ == "__main__":
-    main()
